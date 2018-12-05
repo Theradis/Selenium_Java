83ea51061ba9cf9d0bdaeba4cd2a1bed74af411d
Administrator <Administrator@192.168.1.105>
2018-12-05 14:24:02 -0600

    Added code to PropertiesFile.java
    Now the PropertiesFile.java includes readPropertiesFile() and
    writePropertiesFile() to read and write to the config.propeties file.

--- Selenium_Java/src/config/PropertiesFile.java
+++ Selenium_Java/src/config/PropertiesFile.java
@@ -2,19 +2,27 @@ package config;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
 public class PropertiesFile {
 	
+	//Global Class Variable
+	static Properties prop = new Properties();
+	
 	public static void main(String[] args) {
 		readPropertiesFile();
+		writePropertiesFile();
+	
 	}
 
+	//How to Get Data from Properties File
 	public static void readPropertiesFile() {
 		//Create Properties Object to make available to project
 		//Create InputStream Object to read config data
-		Properties prop = new Properties();
+		
 		try {
 			
 			InputStream input = new FileInputStream("C:\\Users\\Administrator\\GIT REPOSITORY\\Selenium_Java\\Selenium_Java\\src\\config\\config.properties");
@@ -28,4 +36,19 @@ public class PropertiesFile {
 		}
 		
 	}
+	//How to Set Data to Properties File
+	public static void writePropertiesFile() {
+		
+		try {
+			
+			OutputStream output = new FileOutputStream("C:\\\\Users\\\\Administrator\\\\GIT REPOSITORY\\\\Selenium_Java\\\\Selenium_Java\\\\src\\\\config\\\\config.properties");
+			prop.setProperty("browser","Chrome");
+			prop.store(output, "changed browser");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
