a123f0acd03f03d94edf2d8dbce01e31f38e8ec2
Administrator <Administrator@192.168.1.105>
2018-12-05 13:10:53 -0600

    Added Chromedriver to lib folder. Imported chromedriver. Edited FirstSeleniumTest.java to modularize code. Added Function setBrower() Added Function setBrowserConfig() Added Function runTest() and added function calls to each in Main()

--- Selenium_Java/src/test/FirstSeleniumTest.java
+++ Selenium_Java/src/test/FirstSeleniumTest.java
@@ -6,26 +6,58 @@ package test;
 
 //import
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSeleniumTest {
 
+	//Public Global Variables
+	static String browser;
+	static WebDriver driver;
+	
+	
 	public static void main(String[] args) {
+		setBrowser();
+		setBrowserConfig();
+		runTest();
 		
-		//String variable to get path of user.dir  will be usable for program mobility.
+		
+	}
+	
+	public static void setBrowser() {
+		browser ="Firefox";
+	}
+	
+	public static void setBrowserConfig() {
+		//[SET BROWSER]
 		String projectLocation = System.getProperty("user.dir");
+
+		//[SET BROWSER CONFIGURATION]
 		
+		if(browser.contains("Firefox")){
 		//Code for GeckoDriver used for Firefox:  Let application know its using geckodriver and where its located
 		System.setProperty("webdriver.gecko.driver", projectLocation+"\\doc\\geckoDriver\\geckodriver.exe");
 		//Code to specify the browser to be used and where the browser is located.
 		System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
 		
-		WebDriver driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
+		}
 		
+		if(browser.contains("Chrome")) {
+		//Configuration for Chrome
+		//WebDriver driver = new MarionettDriver();
+		
+		System.setProperty("webdriver.chrome.driver", projectLocation+"lib\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		}
+				
+	}
+	
+	public static void runTest() {
+		//[RUN TEST]
 		driver.get("http://seleniumhq.org/");
-		
+
 		driver.quit();
-		
 	}
 
-}
+}//END OF CLASS
